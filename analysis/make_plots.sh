a0cb063459820fe6c0e4925afd1b4356d2e075f3
Max Rausch-Dupont <max@rausch-dupont.de>
2025-03-17 12:36:54 +0100

    Remove old notebooks

--- analysis/make_plots.sh
+++ analysis/make_plots.sh
@@ -1,7 +1,7 @@
-papermill make_plots_combined.ipynb mcl1_mmpbsa.ipynb -p db mcl1_mmpbsa_gb -p TITLE MCL1-MMGBSA
-papermill make_plots_combined.ipynb mcl1_vina.ipynb -p db mcl1_vina -p TITLE MCL1-VINA
+# papermill make_plots_combined.ipynb mcl1_mmpbsa.ipynb -p db mcl1_mmpbsa_gb -p TITLE MCL1-MMGBSA
+# papermill make_plots_combined.ipynb mcl1_vina.ipynb -p db mcl1_vina -p TITLE MCL1-VINA
 papermill make_plots_combined.ipynb enamine10k.ipynb -p db enamine_10k -p TITLE Enamine10k
 papermill make_plots_combined.ipynb enamine50k.ipynb -p db enamine_50k -p TITLE Enamine50k
-papermill make_plots_combined.ipynb mcl1_mmpbsa_precomputed.ipynb -p db mcl1_mmpbsa_gb_precomputed -p TITLE MCL1-MMGBSA
+# papermill make_plots_combined.ipynb mcl1_mmpbsa_precomputed.ipynb -p db mcl1_mmpbsa_gb_precomputed -p TITLE MCL1-MMGBSA
 papermill make_plots_combined.ipynb mcl1_vina_proto.ipynb -p db mcl1_vina_proto -p TITLE MCL1-VINA
 papermill make_plots_combined.ipynb mcl1_mmgbsa_proto.ipynb -p db mcl1_mmpbsa_gb_proto -p TITLE MCL1-MMGBSA
